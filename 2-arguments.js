aca151568f6c7eff593eb83669f14c7f0c321496
Lartey Apo <larteyapo@gmail.com>
2025-09-12 21:57:19 +0000

    Assignment pending

---
+++ 2-arguments.js
@@ -0,0 +1,15 @@
+
+const speak = function (name, time){
+ console.log (`${time} ${name}`)
+};
+
+speak  ('argument', 'No')
+
+const talk = function (age, month){
+    console.log (`${age} ${month}`)
+}
+talk ('Argument', 'found')
+
+const question = function (){
+    console.log ()
+}
