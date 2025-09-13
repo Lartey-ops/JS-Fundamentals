d9477f37e8a90295599d746ca81074b76e709374
Lartey Apo <larteyapo@gmail.com>
2025-09-13 16:34:52 +0000

    Assignment done

---
+++ 5-to_integer.js
@@ -0,0 +1,9 @@
+let args = process.argv.slice(2);
+let input = args[0]
+let number = parseInt(input, 10)
+
+if (isNaN(number)) {
+ console.log ('Not a number')
+} else {
+    console.log (`My number: ${number}`))
+}
